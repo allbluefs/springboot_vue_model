34c0a16661048611c2ca81057746713c7b2da739
fs <942644064@qq.com>
2023-08-10 17:51:16 +0800

    vue3相关配置

--- vue3model/.eslintrc.js
+++ vue3model/.eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "vue/multi-word-component-names": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
